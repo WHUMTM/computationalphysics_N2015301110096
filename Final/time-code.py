8198496bae066411b7bca67297fe0d01d5cdadf1
宇宙理论物理 <31845201+WHUMTM@users.noreply.github.com>
2018-01-15 12:22:14 +0800

    修改时间代码

--- Final/time-code.py
+++ Final/time-code.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 27 20:26:19 2017
+Created on Thu Dec 28 21:16:49 2017
 
 @author: TimMu
 """
+
+
 import matplotlib.pylab as plt
-import  numpy as np
+import numpy as np
 
 class ISING(object):
-    def __init__(self, _H_magnetic=0., _Temperature=0.5, _length=20):
+    def __init__(self, _H_magnetic=0., _Temperature=2, _length=10):
         self.length = int(_length)
         self.H = float(_H_magnetic)
         self.T = float(_Temperature)
@@ -34,11 +36,11 @@ class ISING(object):
             return [self.length-1,self.length-1],[1,self.length-1],[0,self.length-2],[0,0]
         if _posi==[self.length-1,self.length-1]:
             return [self.length-2,self.length-1],[0,self.length-1],[self.length-1,self.length-2],[self.length-1,0]           
-    def MCstep(self,_system, _H=0., _T=0.5):  # sweep all spins -- Monte Carlo step
+    def MCstep(self,_system, _H=0., _T=2):  # sweep all spins -- Monte Carlo step
         for i in range(self.length):
             for j in range(self.length):
                 self.temp1,self.temp2,self.temp3,self.temp4=self.surrounding([i,j])
-                self.temp=list([_system[self.temp1[0]][self.temp1[1]],_system[self.temp2[0]][self.temp2[1]],_system[self.temp3[0]][self.temp3[1]],_system[self.temp4[0]][self.temp4[1]]])
+                self.temp=np.array([_system[self.temp1[0]][self.temp1[1]],_system[self.temp2[0]][self.temp2[1]],_system[self.temp3[0]][self.temp3[1]],_system[self.temp4[0]][self.temp4[1]]])
                 self.delta_e = sum(self.temp)*_system[i][j]*2.+2.*_system[i][j]*_H
                 if self.delta_e <=0:
                     _system[i][j]=-_system[i][j]
@@ -52,7 +54,7 @@ class ISING(object):
         for i in range(self.length):
             for j in range(self.length):
                 self.temp1,self.temp2,self.temp3,self.temp4=self.surrounding([i,j])
-                self.temp=list([_system[self.temp1[0]][self.temp1[1]],_system[self.temp2[0]][self.temp2[1]],_system[self.temp3[0]][self.temp3[1]],_system[self.temp4[0]][self.temp4[1]]])
+                self.temp=np.array([_system[self.temp1[0]][self.temp1[1]],_system[self.temp2[0]][self.temp2[1]],_system[self.temp3[0]][self.temp3[1]],_system[self.temp4[0]][self.temp4[1]]])
                 self.energy=self.energy+(-sum(self.temp)*_system[i][j])/2.- _system[i][j]*_H
         return self.energy/self.length**2
     def plot_system(self,_sys):
@@ -66,19 +68,19 @@ class ISING(object):
             self.mag.append(np.mean(self.system))
             self.ene.append(self.energy_ave(self.system,self.H))
         fig=plt.figure(figsize=(13,12))
-        ax1=plt.axes([0.1,0.2,0.35,0.7])
-        ax2=plt.axes([0.55,0.2,0.35,0.7])
+        ax1=plt.subplot(111)
+        #ax1=plt.axes([0.1,0.2,0.35,0.7])
+        #ax2=plt.axes([0.55,0.2,0.35,0.7])
         ax1.plot(self.time,self.mag,'-b')
-        ax2.plot(self.time,self.ene,'-r')
+        #ax2.plot(self.time,self.ene,'-r')
         ax1.set_xlabel("Time / M-C step",fontsize=15)
         ax1.set_ylabel("Magnetization",fontsize=15)
-        ax2.set_xlabel("Time / M-C step",fontsize=15)
-        ax2.set_ylabel("Energy",fontsize=15)
+        #ax2.set_xlabel("Time / M-C step",fontsize=15)
+        #ax2.set_ylabel("Energy",fontsize=15)
         ax1.set_title("Fluctuation: Magnetization vs. time",fontsize=18)
-        ax2.set_title("Fluctuation: Energy vs. time",fontsize=18)
+       # ax2.set_title("Fluctuation: Energy vs. time",fontsize=18)
         ax1.set_ylim(-1.5,1.5)
-        ax2.set_ylim(-3,3)
+       # ax2.set_ylim(-3,3)
         plt.show()
-cmp=ISING(0,0.5)
-cmp.plot_spinvstime(200)
-    
+cmp=ISING(0,0.9)
+cmp.plot_spinvstime(2000)
